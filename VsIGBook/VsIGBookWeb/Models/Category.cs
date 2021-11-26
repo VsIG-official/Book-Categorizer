6c377762b65488cbd5683c69c7f24c4693f4288e
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-11-26 22:30:32 +0200

    ✨: add ECS attributes

--- VsIGBook/VsIGBookWeb/Models/Category.cs
+++ VsIGBook/VsIGBookWeb/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace VsIGBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VsIGBookWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = "Default Name";
         public int DisplayOrder { get; set; }
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
