c3175b8e6575dfb7c75b92fb6414ab772c234666
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-11-26 22:27:40 +0200

    ✨: add category model

---
+++ VsIGBook/VsIGBookWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace VsIGBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Default Name";
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
