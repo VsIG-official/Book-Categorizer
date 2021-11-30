6ac5b44f11ae332df87ef1d1ab3a9d02a3cd5ce5
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-11-30 17:22:42 +0200

    ✨: update Category page

--- VsIGBook/VsIGBookWeb/Controllers/CategoryController.cs
+++ VsIGBook/VsIGBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VsIGBookWeb.Data;
+using VsIGBookWeb.Models;
 
 namespace VsIGBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> categories = _db.Categories;
+
+            return View(categories);
         }
     }
 }
